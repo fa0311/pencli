aa31867d9153ef51e844e8649748986224d7180d
ふぁ <yuki@yuki0311.com>
2024-09-10 00:18:50 +0900

    init
    Signed-off-by: ふぁ <yuki@yuki0311.com>

---
+++ README.md
@@ -0,0 +1,51 @@
+# pencli
+
+A new CLI generated with oclif
+
+```sh-session
+npx @fa0311/pencli
+```
+
+<!-- toc -->
+* [pencli](#pencli)
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+
+# Usage
+
+<!-- usage -->
+```sh-session
+$ npm install -g @fa0311/pencli
+$ @fa0311/pencli COMMAND
+running command...
+$ @fa0311/pencli (--version)
+@fa0311/pencli/0.0.1 win32-x64 node-v20.17.0
+$ @fa0311/pencli --help [COMMAND]
+USAGE
+  $ @fa0311/pencli COMMAND
+...
+```
+<!-- usagestop -->
+
+# Commands
+
+<!-- commands -->
+* [`@fa0311/pencli`](#fa0311pencli)
+
+## `@fa0311/pencli`
+
+Update installed plugins.
+
+```
+USAGE
+  $ @fa0311/pencli  [-h] [-v]
+
+FLAGS
+  -h, --help     Show CLI help.
+  -v, --verbose
+
+DESCRIPTION
+  Update installed plugins.
+```
+<!-- commandsstop -->
---
+++ src/commands/index.ts
@@ -0,0 +1,45 @@
+import {Args, Command} from '@oclif/core'
+import logUpdate from 'log-update'
+
+const ascii = `
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣷⣄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠲⣄⠙⢿⣿⡿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣤⠈⠳⡄⠉⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠂⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⢁⣴⠟⢁⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢾⣷⣄⠁⠴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣰⠄⠙⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+`
+
+export default class Default extends Command {
+  static args = {
+    text: Args.string({description: 'Text'}),
+  }
+
+  static description = 'Say hello'
+
+  static examples = [`<%= config.bin %> <%= command.id %> Hello`]
+
+  static flags = {}
+
+  async run(): Promise<void> {
+    const {args, flags} = await this.parse(Default)
+
+    if (args.text) {
+      const lines = ascii.split('\n').slice(0, -1)
+      for (let i = 0; i < args.text.length; i++) {
+        const pen = lines.map((line) => line.padStart(i + line.length, ' ')).join('\n')
+        logUpdate(pen + '\n' + args.text.slice(0, i + 1))
+        await new Promise((resolve) => setTimeout(resolve, 100))
+      }
+    } else {
+      this.log(ascii)
+    }
+  }
+}
